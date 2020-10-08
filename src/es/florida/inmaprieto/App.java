a262955ccda081993466765a79deb33289d095a6
Inma Prieto <inmaprieto87@gmail.com>
2020-10-08 13:59:08 +0200

    AE1-1
    Método que muestra Hola Mundo por consola.

---
+++ src/es/florida/inmaprieto/App.java
@@ -0,0 +1,14 @@
+package es.florida.inmaprieto;
+
+public class App {
+
+    public static void main(String[] args) {
+	    App app = new App();
+	    app.sayHello();
+    }
+
+    public void sayHello(){
+        System.out.print("¡Hola Mundo!");
+    }
+
+}
