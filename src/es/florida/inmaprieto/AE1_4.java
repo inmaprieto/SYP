8ade206161d06e1dafd47b92b5d22f40e1654133
Inma Prieto <inmaprieto87@gmail.com>
2020-10-08 15:31:42 +0200

    AE1-4
    Muestra el factorial del número 15 por consola.

---
+++ src/es/florida/inmaprieto/AE1_4.java
@@ -0,0 +1,12 @@
+package es.florida.inmaprieto;
+
+public class AE1_4 {
+
+    public static void main(String[] args) {
+	    int total = 1;
+        for (int i = 1; i <= 15; i++) {
+            total = total * i;
+            System.out.println(total);
+        }
+    }
+}
